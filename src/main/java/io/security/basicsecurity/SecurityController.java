a4c7b62eef29227332c9234812b360ed3d85bb78
유희상 <ddoongmause@lendingmachine.co.kr>
2021-11-02 10:39:44 +0900

    Chapter1 스프링 시큐리티 기본 API 및 Filter 이해까지 완료

---
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -0,0 +1,38 @@
+package io.security.basicsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+    @GetMapping("/")
+    public String index(){
+        return "home";
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin/pay")
+    public String adminPay(){
+        return "adminPay";
+    }
+
+    @GetMapping("/admin/**")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    /*@GetMapping("loginPage")
+    public String loginPage(){
+        return "loginPage";
+    }*/
+}
